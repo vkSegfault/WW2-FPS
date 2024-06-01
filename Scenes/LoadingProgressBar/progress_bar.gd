62ae707e8a1bf5b426bccc89eaa923fef36374e9
vkSegfault <45318345+vkSegfault@users.noreply.github.com>
2024-06-01 16:02:41 +0200

    upd

--- Scenes/LoadingProgressBar/progress_bar.gd
+++ Scenes/LoadingProgressBar/progress_bar.gd
@@ -1,19 +1,39 @@
+##################
+#### Autoload ####
+##################
+
 extends Node
 
-var scene_path: String = "res://Scenes/Levels/SeptemberCampaign/september_campaign_level.tscn"
+signal progress_changed(progress)
+signal load_done
+
+var _load_screen_path: String = "res://Scenes/LoadingProgressBar/loading_progress_bar.tscn"
+var _load_screen = load(_load_screen_path)
+var _loaded_resource: PackedScene
+var _scene_path: String
 var loading_status: int
-var progress: Array[float]
+var progress: Array[float] = []
+var use_sub_threads: bool = true
 
 @onready var progress_bar: ProgressBar = $ProgressBar
 
+
+func load_scene(scene_path: String) -> void:
+	_scene_path = scene_path
+	
+	var load_screen_instance = _load_screen.instantiate()
+	get_tree().get_root().add_child(load_screen_instance)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ResourceLoader.load_threaded_request(scene_path)
+	pass
+	#ResourceLoader.load_threaded_request(scene_path)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	loading_status = ResourceLoader.load_threaded_get_status(scene_path, progress)
+	pass
+	#loading_status = ResourceLoader.load_threaded_get_status(scene_path, progress)
 
 	match loading_status:
 		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
@@ -21,6 +41,6 @@ func _process(delta):
 			progress_bar.value = progress[0] * 100 # Change the ProgressBar value
 		ResourceLoader.THREAD_LOAD_LOADED:
 			print("Scene loaded")
-			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
+			#get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
 		ResourceLoader.THREAD_LOAD_FAILED:
 			print("Error. Could not load Resource")
